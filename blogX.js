1439fa2659c7916edc60c6f1921237b6491a891a
Deepanshu Sarswat <78958441+DeepanshuSarswat@users.noreply.github.com>
2021-04-21 22:03:41 -0700

    Add files via upload

---
+++ blogX.js
@@ -0,0 +1,5 @@
+var toggleMenu = document.querySelector("#toggle");
+var nav = document.querySelector(".nav");
+toggleMenu.addEventListener("click", function () {
+  nav.classList.toggle("nstart");
+});
